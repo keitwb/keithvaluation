b6bc86ed7e50a3f5c0fc496d2d45b9361a59136c
Ben Keith <keitwb@gmail.com>
2018-04-15 18:31:57 -0400

    Adding more worker threads

--- docker/app/gunicorn-config.py
+++ docker/app/gunicorn-config.py
@@ -3,6 +3,6 @@ import multiprocessing
 user = 'django'
 bind = '0.0.0.0:8000'
 errorlog = '/logs/django/gunicorn.error.log'
-workers = 2 * multiprocessing.cpu_count()
+workers = 4 * multiprocessing.cpu_count()
 max_requests = 1000
 max_requests_jitter = 50
